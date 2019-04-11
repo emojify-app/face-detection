d6c39ed09fedf4387361022f10c9ee2199617f5f
Nicholas Jackson <jackson.nic@gmail.com>
2019-04-11 07:22:24 +0100

    Add profiling option

--- main.go
+++ main.go
@@ -17,8 +17,9 @@ import (
 var listenAddress = env.String("BIND_ADDRESS", false, "127.0.0.1", "Listen address for the server")
 var listenPort = env.String("BIND_PORT", false, "9090", "Listen port for the server")
 var statsDAddress = env.String("STATSD_ADDRESS", false, "localhost:8125", "Location of the statsd collector")
-var logLevel = env.String("LOG_LEVEL", false, "info", "Log level [info,debug,trace]")
+var logLevel = env.String("LOG_LEVEL", false, "info", "log level [info,debug,trace]")
 var cascadeFolder = env.String("CASCADE_FOLDER", false, "./cascades", "location of the OpenCV cascades")
+var profile = env.Bool("PROFILE", false, false, "enable pprof profiling")
 
 func main() {
 	// Parse the config env vars
@@ -50,16 +51,18 @@ func main() {
 	r.Handle("/health", h).Methods("GET")
 	r.Handle("/", fd).Methods("POST")
 
-	// Register pprof handlers
-	r.HandleFunc("/debug/pprof/", pprof.Index)
-	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	if *profile {
+		// Register pprof handlers
+		r.HandleFunc("/debug/pprof/", pprof.Index)
+		r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 
-	r.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-	r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	r.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
-	r.Handle("/debug/pprof/block", pprof.Handler("block"))
+		r.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+		r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		r.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+		r.Handle("/debug/pprof/block", pprof.Handler("block"))
+	}
 
 	l.ServiceStart(*listenAddress, *listenPort)
 	fmt.Println("Error starting server", "error", http.ListenAndServe(fmt.Sprintf("%s:%s", *listenAddress, *listenPort), r))
